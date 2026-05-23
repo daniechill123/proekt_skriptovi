60d00e4ea3b36545deda55e3709be6762db2553e
daniechill123 <daniechill6@gmail.com>
2026-05-23 18:12:59 +0300

    fixed prev commit

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        password = 'root123',
-        database = 'school_diary'
+        host='localhost',
+        user='root',
+        password='root123456789',
+        database='school_diary'
     )
+    return connection
